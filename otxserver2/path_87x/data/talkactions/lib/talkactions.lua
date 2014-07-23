029fba9751e22f1fc26e85daaed9119bcb06f011
mattyx14 <mattyx14@users.noreply.github.com>
2014-07-23 11:58:22 -0500

    Prevent spam commands add this to commands if(not checkExhausted(cid, 666, 10)) then 	return false end

--- otxserver2/path_87x/data/talkactions/lib/talkactions.lua
+++ otxserver2/path_87x/data/talkactions/lib/talkactions.lua
@@ -1,1 +1,12 @@
--- Nothing --
+-- Prevent spamm commands --
+function checkExhausted(cid, storage, seconds)
+	local v = exhaustion.get(cid, storage)
+	if(not v) then
+		exhaustion.set(cid, storage, seconds)
+	else
+		doPlayerSendTextMessage(cid, MESSAGE_EVENT_DEFAULT, "Please wait " .. v .. " seconds before trying this command again.")
+		return false
+	end
+
+	return true
+end
