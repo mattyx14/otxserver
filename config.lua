acb569d5aed62b881c8429c6d0239ebc672ed931
mattyx14 <mattyx14@users.noreply.github.com>
2014-11-08 19:21:39 -0600

    my config values

--- config.lua
+++ config.lua
@@ -5,12 +5,12 @@ hotkeyAimbotEnabled = "yes"
 protectionLevel = 1
 killsToRedSkull = 3
 killsToBlackSkull = 6
-pzLocked = 60000
+pzLocked = 60 * 1000
 removeAmmoWhenUsingDistanceWeapon = "yes"
 removeChargesFromRunes = "yes"
 timeToDecreaseFrags = 24 * 60 * 60 * 1000
 whiteSkullTime = 15 * 60 * 1000
-stairJumpExhaustion = 2000
+stairJumpExhaustion = 2 * 1000
 experienceByKillingPlayers = "no"
 expFromPlayersLevelRange = 75
 noDamageToSameLookfeet = "no"
@@ -23,11 +23,11 @@ loginProtocolPort = 7171
 gameProtocolPort = 7172
 statusProtocolPort = 7171
 maxPlayers = 0
-motd = "Welcome to The Forgotten Server!"
+motd = "Welcome to DarkKonia"
 onePlayerOnlinePerAccount = "yes"
 allowClones = "no"
-serverName = "Forgotten"
-statusTimeout = 5000
+serverName = "DarkKonia"
+statusTimeout = 1 * 1000
 replaceKickOnLogin = "yes"
 maxPacketsPerSecond = 25
 
@@ -40,7 +40,7 @@ deathLosePercent = -1
 -- Houses
 -- NOTE: set housePriceEachSQM to -1 to disable the ingame buy house functionality
 housePriceEachSQM = 1000
-houseRentPeriod = "never"
+houseRentPeriod = "weekly"
 
 -- Item Usage
 timeBetweenActions = 200
@@ -48,8 +48,8 @@ timeBetweenExActions = 1000
 
 -- Map
 -- NOTE: set mapName WITHOUT .otbm at the end
-mapName = "forgotten"
-mapAuthor = "Komic"
+mapName = "darkkonia"
+mapAuthor = "Mattyx14"
 
 -- Market
 marketOfferDuration = 30 * 24 * 60 * 60
@@ -59,9 +59,9 @@ maxMarketOffersAtATimePerPlayer = 100
 
 -- MySQL
 mysqlHost = "127.0.0.1"
-mysqlUser = "forgottenserver"
+mysqlUser = ""
 mysqlPass = ""
-mysqlDatabase = "forgottenserver"
+mysqlDatabase = ""
 mysqlPort = 3306
 mysqlSock = ""
 
@@ -95,10 +95,10 @@ convertUnsafeScripts = "no"
 -- Startup
 -- NOTE: defaultPriority only works on Windows and sets process priority.
 defaultPriority = "high"
-startupDatabaseOptimization = "no"
+startupDatabaseOptimization = "yes"
 
 -- Status server information
-ownerName = ""
-ownerEmail = ""
-url = "http://otland.net/"
-location = "Sweden"
+ownerName = "Mattyx14"
+ownerEmail = "darkylive@live.com.mx"
+url = "https://github.com/mattyx14"
+location = "Mexico"
