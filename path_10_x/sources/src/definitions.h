b0704656a2e2d173905b07d6dfc96c6a17839e8b
mattyx14 <mattyx14@users.noreply.github.com>
2015-10-13 17:20:28 -0500

    10.82 support (no include items.otb ... it use official 10.7x items)

--- path_10_x/sources/src/definitions.h
+++ path_10_x/sources/src/definitions.h
@@ -28,9 +28,9 @@
 #define STATUS_SERVER_DEVELOPERS "Mattyx14 and TFS Developers"
 #define MAIN_FORUM "http://www.blacktibia.org/f19-otx-server-3"
 
-#define CLIENT_VERSION_MIN 1076
-#define CLIENT_VERSION_MAX 1077
-#define CLIENT_VERSION_STR "10.77"
+#define CLIENT_VERSION_MIN 1082
+#define CLIENT_VERSION_MAX 1082
+#define CLIENT_VERSION_STR "10.82"
 
 #ifndef __FUNCTION__
 #define __FUNCTION__ __func__
