fce44d4637d749db3d25f25ffbd8db5c51d89c7c
mattyx14 <mattyx14@users.noreply.github.com>
2015-10-26 11:07:30 -0600

    Fix bug with market offers Fix bed on 8x

--- path_10_8/src/protocolgame.cpp
+++ path_10_8/src/protocolgame.cpp
@@ -2391,7 +2391,7 @@ void ProtocolGame::sendAddCreature(const Creature* creature, const Position& pos
 	msg.addByte(0x00); // expert mode button enabled
 
 	msg.addString("http://static.tibia.com/images/store/");
-	msg.addByte(3);
+	msg.addByte(g_config.getNumber(ConfigManager::MAX_PACKETS_PER_SECOND));
 
 	writeToOutputBuffer(msg);
 
