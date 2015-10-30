b25655db8686b687f81f429dea15fca810db2ca5
mattyx14 <mattyx14@users.noreply.github.com>
2015-10-29 18:52:31 -0600

    test

--- path_8_7/config.lua
+++ path_8_7/config.lua
@@ -28,7 +28,7 @@ allowWalkthrough = true
 serverName = "OTXSERVER"
 statusTimeout = 1 * 1000
 replaceKickOnLogin = true
-maxPacketsPerSecond = 25
+maxPacketsPerSecond = 50
 
 -- Deaths
 -- NOTE: Leave deathLosePercent as -1 if you want to use the default
