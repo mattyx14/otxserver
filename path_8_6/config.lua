a6e6d2740e508688c770bdf936af33d9345915b9
mattyx14 <mattyx14@users.noreply.github.com>
2015-10-31 15:48:31 -0600

    format to config

--- path_8_6/config.lua
+++ path_8_6/config.lua
@@ -1,98 +1,100 @@
--- Combat settings
--- NOTE: valid values for worldType are: "pvp", "no-pvp" and "pvp-enforced"
-worldType = "pvp"
-hotkeyAimbotEnabled = true
-protectionLevel = 1
-killsToRedSkull = 3
-killsToBlackSkull = 6
-pzLocked = 60000
-removeChargesFromRunes = true
-timeToDecreaseFrags = 24 * 60 * 60 * 1000
-whiteSkullTime = 15 * 60 * 1000
-stairJumpExhaustion = 2 * 1000
-experienceByKillingPlayers = false
-expFromPlayersLevelRange = 75
+-- The OTX Server Config
 
--- Connection Config
--- NOTE: maxPlayers set to 0 means no limit
-ip = "127.0.0.1"
-bindOnlyGlobalAddress = false
-loginProtocolPort = 7171
-gameProtocolPort = 7172
-statusProtocolPort = 7171
-maxPlayers = 0
-motd = "Welcome to The OTX Server!"
-onePlayerOnlinePerAccount = true
-allowClones = false
-allowWalkthrough = true
-serverName = "OTXSERVER"
-statusTimeout = 1 * 1000
-replaceKickOnLogin = true
-maxPacketsPerSecond = 50
+	-- Combat settings
+	-- NOTE: valid values for worldType are: "pvp", "no-pvp" and "pvp-enforced"
+	worldType = "pvp"
+	hotkeyAimbotEnabled = true
+	protectionLevel = 1
+	killsToRedSkull = 3
+	killsToBlackSkull = 6
+	pzLocked = 1 * 60 * 1000
+	removeChargesFromRunes = true
+	timeToDecreaseFrags = 24 * 60 * 60 * 1000
+	whiteSkullTime = 15 * 60 * 1000
+	stairJumpExhaustion = 2 * 1000
+	experienceByKillingPlayers = false
+	expFromPlayersLevelRange = 75
 
--- Deaths
--- NOTE: Leave deathLosePercent as -1 if you want to use the default
--- death penalty formula. For the old formula, set it to 10. For
--- no skill/experience loss, set it to 0.
-deathLosePercent = -1
+	-- Connection Config
+	-- NOTE: maxPlayers set to 0 means no limit
+	ip = "127.0.0.1"
+	bindOnlyGlobalAddress = false
+	loginProtocolPort = 7171
+	gameProtocolPort = 7172
+	statusProtocolPort = 7171
+	maxPlayers = 0
+	motd = "Welcome to The OTX Server!"
+	onePlayerOnlinePerAccount = true
+	allowClones = false
+	allowWalkthrough = true
+	serverName = "OTXSERVER"
+	statusTimeout = 1 * 1000
+	replaceKickOnLogin = true
+	maxPacketsPerSecond = 25
 
--- Houses
--- NOTE: set housePriceEachSQM to -1 to disable the ingame buy house functionality
-housePriceEachSQM = 1000
-houseRentPeriod = "never"
+	-- Deaths
+	-- NOTE: Leave deathLosePercent as -1 if you want to use the default
+	-- death penalty formula. For the old formula, set it to 10. For
+	-- no skill/experience loss, set it to 0.
+	deathLosePercent = -1
 
--- Item Usage
-timeBetweenActions = 200
-timeBetweenExActions = 1000
+	-- Houses
+	-- NOTE: set housePriceEachSQM to -1 to disable the ingame buy house functionality
+	housePriceEachSQM = 1000
+	houseRentPeriod = "never"
 
--- Map
--- NOTE: set mapName WITHOUT .otbm at the end
-mapName = "forgotten"
-mapAuthor = "Komic"
+	-- Item Usage
+	timeBetweenActions = 200
+	timeBetweenExActions = 1000
 
--- MySQL
-mysqlHost = "127.0.0.1"
-mysqlUser = "root"
-mysqlPass = "master"
-mysqlDatabase = "darkkonia"
-mysqlPort = 3306
-mysqlSock = ""
+	-- Map
+	-- NOTE: set mapName WITHOUT .otbm at the end
+	mapName = "forgotten"
+	mapAuthor = "Komic"
 
--- Misc.
-allowChangeOutfit = true
-freePremium = false
-kickIdlePlayerAfterMinutes = 15
-maxMessageBuffer = 4
-emoteSpells = true
-classicEquipmentSlots = false
+	-- MySQL
+	mysqlHost = "127.0.0.1"
+	mysqlUser = "root"
+	mysqlPass = "master"
+	mysqlDatabase = "darkkonia"
+	mysqlPort = 3306
+	mysqlSock = ""
 
--- Rates
--- NOTE: rateExp is not used if you have enabled stages in data/XML/stages.xml
-rateExp = 1
-rateSkill = 3
-rateLoot = 2
-rateMagic = 3
-rateSpawn = 1
+	-- Misc.
+	allowChangeOutfit = true
+	freePremium = false
+	kickIdlePlayerAfterMinutes = 15
+	maxMessageBuffer = 4
+	emoteSpells = true
+	classicEquipmentSlots = false
 
--- Monsters
-deSpawnRange = 2
-deSpawnRadius = 50
+	-- Rates
+	-- NOTE: rateExp is not used if you have enabled stages in data/XML/stages.xml
+	rateExp = 1
+	rateSkill = 3
+	rateLoot = 2
+	rateMagic = 3
+	rateSpawn = 1
 
--- Stamina
-staminaSystem = true
+	-- Monsters
+	deSpawnRange = 2
+	deSpawnRadius = 50
 
--- Scripts
-warnUnsafeScripts = true
-convertUnsafeScripts = true
+	-- Stamina
+	staminaSystem = true
 
--- Startup
--- NOTE: defaultPriority only works on Windows and sets process
--- priority, valid values are: "normal", "above-normal", "high"
-defaultPriority = "high"
-startupDatabaseOptimization = true
+	-- Scripts
+	warnUnsafeScripts = true
+	convertUnsafeScripts = true
 
--- Status server information
-ownerName = "Mattyx14"
-ownerEmail = "darkylive@live.com.mx"
-url = "https://github.com/mattyx14"
-location = "Mexico"
+	-- Startup
+	-- NOTE: defaultPriority only works on Windows and sets process
+	-- priority, valid values are: "normal", "above-normal", "high"
+	defaultPriority = "high"
+	startupDatabaseOptimization = true
+
+	-- Status server information
+	ownerName = "Mattyx14"
+	ownerEmail = "darkylive@live.com.mx"
+	url = "https://github.com/mattyx14"
+	location = "Mexico"
