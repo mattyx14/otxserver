27d6d4a31312b80703c6e1c4b07490586517e3c6
mattyx14 <mattyx14@users.noreply.github.com>
2016-05-01 16:17:55 -0500

    missing it

--- path_8_7x/sources/npc.cpp
+++ path_8_7x/sources/npc.cpp
@@ -1980,6 +1980,9 @@ void Npc::onPlayerTrade(Player* player, ShopEvent_t type, int32_t callback, uint
 	{
 		if(NpcState* npcState = getState(player, true))
 		{
+			if(amount <= 0)
+				amount = 1;
+
 			npcState->amount = amount;
 			npcState->subType = count;
 			npcState->itemId = itemId;
