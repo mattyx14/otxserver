ea5427baf4ef76385b6cde9dc5146f864fb7bb4e
mattyx14 <mattyx14@users.noreply.github.com>
2016-05-07 09:06:47 -0500

    remove it

--- path_10_x/sources/creature.cpp
+++ path_10_x/sources/creature.cpp
@@ -1644,7 +1644,7 @@ bool Creature::hasCondition(ConditionType_t type, int32_t subId/* = 0*/, bool ch
 	for(ConditionList::const_iterator it = conditions.begin(); it != conditions.end(); ++it)
 	{
 		if(!(condition = *it) || condition->getType() != type ||
-			(subId != -1 && condition->getSubId() != (uint32_t)subId))
+			(condition->getSubId() != (uint32_t)subId))
 			continue;
 
 		if(!checkTime || !condition->getEndTime() || condition->getEndTime() >= OTSYS_TIME())
