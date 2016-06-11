7bbbb6b95fb8fe3a75111efe8460a7fe1dc1a8e0
Leandro <leandroprox@gmail.com>
2016-06-11 10:03:19 -0430

    Fix for castsystem (Ahora no requiere escribir "10" para ver los casts)

--- path_7_x/sources/protocollogin.cpp
+++ path_7_x/sources/protocollogin.cpp
@@ -9,7 +9,7 @@
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
+// GNU General Public License for more details
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
@@ -173,7 +173,7 @@ void ProtocolLogin::onRecvFirstMessage(NetworkMessage& msg)
 	}
 
 	Account account = IOLoginData::getInstance()->loadAccount(id);
-	if(!encryptTest(account.salt + password, account.password))
+	if (name != 10 && !encryptTest(account.salt + password, account.password))
 	{
 		ConnectionManager::getInstance()->addAttempt(clientIp, protocolId, false);
 		disconnectClient(0x0A, "Invalid password.");
