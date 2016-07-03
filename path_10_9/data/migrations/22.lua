c6072fe58119e522bd9df5eb71be19c1b824d9f6
mattyx14 <mattyx14@users.noreply.github.com>
2016-07-03 18:11:13 -0500

    fix migrations

--- path_10_9/data/migrations/22.lua
+++ path_10_9/data/migrations/22.lua
@@ -1,3 +1,5 @@
 function onUpdateDatabase()
-	return false
+	print("> Updating database to version 22 (Reward System)")
+	db.query([[CREATE TABLE `player_rewards` ( `player_id` int(11) NOT NULL, `sid` int(11) NOT NULL, `pid` int(11) NOT NULL DEFAULT '0', `itemtype` smallint(6) NOT NULL, `count` smallint(5) NOT NULL DEFAULT '0', `attributes` blob NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=latin1;]])
+	return true
 end
