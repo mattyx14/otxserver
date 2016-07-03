763376d2e4978f4eded54841a48cabde30849a28
mattyx14 <mattyx14@users.noreply.github.com>
2016-07-03 14:42:36 -0500

    Start to OTX Server 3.6 :: Version " SCARETALE "

--- path_8_7/src/definitions.h
+++ path_8_7/src/definitions.h
@@ -22,9 +22,9 @@
 
 #define STATUS_SERVER_NAME "OTX Server"
 #define STATUS_SERVER_VERSION "3"
-#define MINOR_VERSION "5"
-#define REVISION_VERSION "673"
-#define SOFTWARE_CODENAME "FORGOTTEN"
+#define MINOR_VERSION "6"
+#define REVISION_VERSION "-"
+#define SOFTWARE_CODENAME "SCARETALE"
 #define STATUS_SERVER_DEVELOPERS "Mattyx14, Brewsterl and The Forgotten Server Developers"
 #define GIT_REPO "https://github.com/mattyx14/otxserver/tree/otxserv3"
 
