36dc00c7c6e139ff79e23273d45328554cd20ee3
mattyx14 <mattyx14@users.noreply.github.com>
2016-11-04 13:58:00 -0600

    Fix datapacks

--- path_8_7/data/actions/scripts/liquids/potions.lua
+++ path_8_7/data/actions/scripts/liquids/potions.lua
@@ -1,215 +1,102 @@
---[[
-	-- Don't forget to put new potions in actions.xml in addition to adding here!
-	Values of potions are copied from tibia wiki!
-	-- Explanations!
-	effect : effect to send to player after using potion! Default : CONST_ME_MAGIC_BLUE (required)
-	removePotions : removing the potion after use. Default : true (optional)
-	addEmptyFlasks : add the empty flask after using! ( only works if removePotions ) Default : true (optional)
-	words : to say after using! Default : "Aaaah..." (optional)
-	healthRate : affects the formula of health/mana added (min * healthRate), (max * healthRate) -- Default: 1 (required)
-	manaRate : same as healthRate but, for mana -- Default: 1 (required)
-	-- For every potion!
-	[potionId] = {
-		vocations : the vocations that are able to use the potion! (optional)
-		minLevel : minimum level to use the potion (optional)
-		health/mana : (required)
-			min, max : the Formula of adding health (i.e , the potion addes from 150 ~ 200 health) (required)
-		emptyFlask : the empty flask to add to player (optional)
-	}
-]]
-local potions = {
-	-- These values are default values in tibia, copied from tibiawiki.
-	effect = CONST_ME_MAGIC_GREEN,
-	removePotions = true,
-	addEmptyFlasks = true,
-	words = "Aaaah...",
-	healthRate = 1,
-	manaRate = 1,
-	-- string to say if player can't use the potion!
-	-- First skip is for vocations(string), second skip is for level(int)!
-	stringOfVocation = "This potion can only be consumed by %s of level %d or higher", -- (Required*)
-	-- Health & Spirit Potions
-	[8473] = { -- ultimate health potion
-		vocations = {4, 8},
-		minLevel = 130,
-		healthToAdd = { min = 650, max = 850 },
-		emptyFlask = 7635
-	},
-	[7591] = { -- great health potion
-		vocations = {4, 8},
-		minLevel = 80,
-		healthToAdd = { min = 425, max = 575},
-		emptyFlask = 7635
-	},
-	[8472] = { -- great spitit potion
-		vocations = {3, 7},
-		minLevel = 80,
-		healthToAdd = { min = 250, max = 350},
-		manaToAdd = { min = 100, max = 200},
-		emptyFlask = 7635
-	},
-	[7588] = { -- strong health potion
-		vocations = {3, 4, 7, 8},
-		minLevel = 50,
-		healthToAdd = { min = 250, max = 350},
-		emptyFlask = 7634
-	},
-	[7618] = { -- health potion
-		healthToAdd = { min = 125, max = 175},
-		emptyFlask = 7636
-	},
-	-- Mana Potions
-	[7590] = { -- great mana potion
-		vocations = {1, 2, 5, 6},
-		minLevel = 80,
-		manaToAdd = { min = 150, max = 250},
-		emptyFlask = 7635
-	},
-	[7589] = { -- strong mana potion
-		vocations = {1, 2, 3, 5, 6, 7},
-		minLevel = 50,
-		manaToAdd = { min = 115, max = 185},
-		emptyFlask = 7634
-	},
-	[7620] = { -- mana potion
-		manaToAdd = { min = 75, max = 125},
-		emptyFlask = 7636
-	},
-	[8704] = { -- small health potion
-		healthToAdd = { min = 60, max = 90},
-		emptyFlask = 7636
-	}
-}
---[[ available custom methods!
-	combat : MetatableCombat
-	condition : MetatableCondition (not used)
-]]
--- antidote
+local berserk = Condition(CONDITION_ATTRIBUTES)
+berserk:setParameter(CONDITION_PARAM_SUBID, 7)
+berserk:setParameter(CONDITION_PARAM_TICKS, 10 * 60 * 1000)
+berserk:setParameter(CONDITION_PARAM_SKILL_MELEE, 5)
+berserk:setParameter(CONDITION_PARAM_SKILL_SHIELD, -10)
+
+local mastermind = Condition(CONDITION_ATTRIBUTES)
+mastermind:setParameter(CONDITION_PARAM_SUBID, 8)
+mastermind:setParameter(CONDITION_PARAM_TICKS, 10 * 60 * 1000)
+mastermind:setParameter(CONDITION_PARAM_STAT_MAGICPOINTS, 3)
+
+local bullseye = Condition(CONDITION_ATTRIBUTES)
+bullseye:setParameter(CONDITION_PARAM_SUBID, 9)
+bullseye:setParameter(CONDITION_PARAM_TICKS, 10 * 60 * 1000)
+bullseye:setParameter(CONDITION_PARAM_SKILL_DISTANCE, 5)
+bullseye:setParameter(CONDITION_PARAM_SKILL_SHIELD, -10)
+
 local antidote = Combat()
 antidote:setParameter(COMBAT_PARAM_TYPE, COMBAT_HEALING)
 antidote:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
-antidote:setParameter(COMBAT_PARAM_TARGETCASTERORTOPMOST, true)
-antidote:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 antidote:setParameter(COMBAT_PARAM_DISPEL, CONDITION_POISON)
-potions[8474] = {combat = antidote, emptyFlask = 7636}
+antidote:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
+antidote:setParameter(COMBAT_PARAM_TARGETCASTERORTOPMOST, true)
 
---- FUNCTIONS ---
+local potions = {
+	[6558] = {transform = {id = {7588, 7589}}, effect = CONST_ME_DRAWBLOOD},
+	[7439] = {condition = berserk, vocations = {4, 8}, effect = CONST_ME_MAGIC_RED,
+			description = "Only knights may drink this potion.", text = "You feel stronger."},
 
-potions.exhaust = Condition(CONDITION_EXHAUST_HEAL)
-potions.exhaust:setParameter(CONDITION_PARAM_TICKS, (configManager.getNumber(configKeys.EX_ACTIONS_DELAY_INTERVAL) - 100))
--- 1000 - 100 due to exact condition timing. -100 doesn't hurt us, and players don't have reminding ~50ms exhaustion.
+	[7440] = {condition = mastermind, vocations = {1, 2, 5, 6}, effect = CONST_ME_MAGIC_BLUE,
+			description = "Only sorcerers and druids may drink this potion.", text = "You feel smarter."},
+
+	[7443] = {condition = bullseye, vocations = {3, 7}, effect = CONST_ME_MAGIC_GREEN,
+			description = "Only paladins may drink this potion.", text = "You feel more accurate."},
+
+	[7588] = {health = {250, 350}, vocations = {3, 4, 7, 8}, level = 50, flask = 7634,
+			description = "Only knights and paladins of level 50 or above may drink this fluid."},
+
+	[7589] = {mana = {115, 185}, vocations = {1, 2, 3, 5, 6, 7}, level = 50, flask = 7634,
+			description = "Only sorcerers, druids and paladins of level 50 or above may drink this fluid."},
+
+	[7590] = {mana = {150, 250}, vocations = {1, 2, 5, 6}, level = 80, flask = 7635,
+			description = "Only druids and sorcerers of level 80 or above may drink this fluid."},
+
+	[7591] = {health = {425, 575}, vocations = {4, 8}, level = 80, flask = 7635,
+			description = "Only knights of level 80 or above may drink this fluid."},
+
+	[7618] = {health = {125, 175}, flask = 7636},
+	[7620] = {mana = {75, 125}, flask = 7636},
+	[8472] = {health = {250, 350}, mana = {100, 200}, vocations = {3, 7}, level = 80, flask = 7635,
+			description = "Only paladins of level 80 or above may drink this fluid."},
+
+	[8473] = {health = {650, 850}, vocations = {4, 8}, level = 130, flask = 7635,
+			description = "Only knights of level 130 or above may drink this fluid."},
+
+	[8474] = {combat = antidote, flask = 7636},
+	[8704] = {health = {60, 90}, flask = 7636}
+}
 
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if target == nil or not target:isPlayer() then
+	if type(target) == "userdata" and not target:isPlayer() then
 		return false
 	end
 
 	local potion = potions[item:getId()]
-	if potion then
-		if (potion.vocations or potion.minLevel) and not getPlayerFlagValue(player, PlayerFlag_IgnoreSpellCheck) then
-			potion.minLevel = math.max(potion.minLevel, 1)
-
-			if not potion.vocations then
-				potion.vocations = { [1] = player:getVocation():getId() }
-			end
-
-			if not isInArray(potion.vocations, player:getVocation():getId()) or player:getLevel() < potion.minLevel then
-				local vocationString, vocations = "", {}
-				-- We only show unpromoted vocations in description.
-				for key, vocationId in ipairs(potion.vocations) do
-					if not Vocation(vocationId):getDemotion() then
-						table.insert(vocations, vocationId)
-					end
-				end
-
-				-- If we prefered to only put promoted vocations, we only show those who are lowered!
-				-- Instead of showing (elite knigts, epic knights, ..), only shows (elite knights)
-				-- It don't get affected by adding different vocations!
-				if #vocations == 0 then
-					for key, vocationId in ipairs(potion.vocations) do
-						if not isInArray(potion.vocations, Vocation(vocationId):getDemotion():getId()) then
-							table.insert(vocations, vocationId)
-						end
-					end
-				end
-
-				for i = 1, #vocations do
-					local stringPtr
-					if i == #vocations then
-						stringPtr = #vocations ~= 1 and " and " or ""
-					else
-						stringPtr = i ~= 1 and ", " or ""
-					end
-
-					vocationString = vocationString .. stringPtr .. Vocation(vocations[i]):getName():lower() .. "s"
-				end
-
-				if vocationString:len() == 0 then
-					vocationString = "players"
-				end
-
-				if not potion.stringOfVocation then
-					potion.stringOfVocation = "This potion can only be consumed by %s of level %d or higher"
-				end
-
-				return not player:say(string.format(potions.stringOfVocation, vocationString, potion.minLevel), TALKTYPE_MONSTER_SAY)
-			end
-		end
-
-		-- Condition exists after the check!
-		if player:getCondition(CONDITION_EXHAUST_HEAL) then
-			return player:sendTextMessage(MESSAGE_STATUS_SMALL, Game.getReturnMessage(RETURNVALUE_YOUAREEXHAUSTED))
-		end
-
-		local effect = potions.effect or CONST_ME_MAGIC_BLUE
-
-		if potion.healthToAdd then
-			local min, max = potion.healthToAdd.min * (potions.healthRate or 1), potion.healthToAdd.max * (potions.healthRate or 1)
-			if not doTargetCombatHealth(0, target, COMBAT_HEALING, min, max, effect) then
-				return false
-			end
-		end
-
-		if potion.manaToAdd then
-			local min, max = potion.manaToAdd.min * (potions.manaRate or 1), potion.manaToAdd.max * (potions.manaRate or 1)
-			if not doTargetCombatMana(0, target, min, max, effect) then
-				return false
-			end
-		end
-
-		if potion.combat and not potion.combat:execute(target, numberToVariant(target:getId())) then
-			return false
-		end
-
-		if potion.condition and potion.condition:getType() ~= CONDITION_EXHAUST_HEAL and not player:addCondition(potion.condition) then
-			return false
-		end
-
-		if not potions.words then
-			potions.words = "Aaaah..."
-		end
-
-		target:say(potions.words, TALKTYPE_MONSTER_SAY)
-
-		-- The default behavior is removing even if nil.
-		if potions.removePotions ~= false then
-			if potions.addEmptyFlasks ~= false and potion.emptyFlask then
-				-- potion's empty potion should be in player's backpack only if the potion was in his backpack!
-				if item:getTopParent().uid == player.uid then
-					player:addItem(potion.emptyFlask)
-				else
-					Game.createItem(potion.emptyFlask, 1, item:getPosition())
-				end
-			end
-
-			-- Removing potion at the end to avoid <nil> error.
-			item:remove(1)
-		end
-
-		if not getPlayerFlagValue(player, PlayerFlag_HasNoExhaustion) then
-			player:addCondition(potions.exhaust) -- Last thing to do is adding condition!
-		end
+	if potion.level and player:getLevel() < potion.level or potion.vocations and not isInArray(potion.vocations, player:getVocation():getId()) then
+		player:say(potion.description, TALKTYPE_MONSTER_SAY)
 		return true
 	end
-	return false -- you can't use this object (potion isn't added to the table.)
+
+	if potion.health or potion.mana or potion.combat then
+		if potion.health then
+			doTargetCombatHealth(0, target, COMBAT_HEALING, potion.health[1], potion.health[2], CONST_ME_MAGIC_BLUE)
+		end
+
+		if potion.mana then
+			doTargetCombatMana(0, target, potion.mana[1], potion.mana[2], CONST_ME_MAGIC_BLUE)
+		end
+
+		if potion.combat then
+			potion.combat:execute(target, Variant(target:getId()))
+		end
+
+		target:say("Aaaah...", TALKTYPE_MONSTER_SAY)
+		player:addItem(potion.flask, 1)
+	end
+
+	if potion.condition then
+		player:addCondition(potion.condition)
+		player:say(potion.text, TALKTYPE_MONSTER_SAY)
+		player:getPosition():sendMagicEffect(potion.effect)
+	end
+
+	if potion.transform then
+		item:transform(potion.transform.id[math.random(#potion.transform.id)])
+		item:getPosition():sendMagicEffect(potion.effect)
+		return true
+	end
+
+	item:remove(1)
+	return true
 end
