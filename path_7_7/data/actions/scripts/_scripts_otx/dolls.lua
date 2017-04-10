fcfefe9fcf91551aec363a3f780d362f50228641
Leandro <leandroprox@gmail.com>
2017-04-09 23:12:27 -0400

    Detallitos

--- path_7_7/data/actions/scripts/_scripts_otx/dolls.lua
+++ path_7_7/data/actions/scripts/_scripts_otx/dolls.lua
@@ -14,21 +14,6 @@ function onUse(player, item, fromPosition, target, toPosition)
 
 	local random = math.random(#sounds)
 	local sound = sounds[random]
-	if item.itemid == 6566 then
-		if random == 3 then
-			fromPosition:sendMagicEffect(CONST_ME_POFF)
-		elseif random == 4 then
-			fromPosition:sendMagicEffect(CONST_ME_FIREAREA)
-		elseif random == 5 then
-			doTargetCombatHealth(0, player, COMBAT_PHYSICALDAMAGE, -1, -1, CONST_ME_EXPLOSIONHIT)
-		end
-	elseif item.itemid == 5669 then
-		fromPosition:sendMagicEffect(CONST_ME_MAGIC_RED)
-		item:transform(item.itemid + 1)
-		item:decay()
-	elseif item.itemid == 6388 then
-		fromPosition:sendMagicEffect(CONST_ME_SOUND_YELLOW)
-	end
 
 	sound = sound:gsub('|PLAYERNAME|', player:getName())
 	player:say(sound, TALKTYPE_MONSTER_SAY, false, 0, fromPosition)
