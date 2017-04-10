af338163cb249c73fd7eae2807d1d6a051dfda55
João Leonardo Pereira <jlcvp@users.noreply.github.com>
2017-04-10 11:55:50 -0300

    Revert "new critical damage calculations"

--- path_10_9/src/enums.h
+++ path_10_9/src/enums.h
@@ -571,14 +571,11 @@ struct CombatDamage
 	} primary, secondary;
 
 	CombatOrigin origin;
-	bool critical;
-
 	CombatDamage()
 	{
 		origin = ORIGIN_NONE;
 		primary.type = secondary.type = COMBAT_NONE;
 		primary.value = secondary.value = 0;
-		critical = false;
 	}
 };
 
