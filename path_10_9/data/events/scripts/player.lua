da192564f40cc7924529222367bef470ac5877d7
Matt Gomez <mattyx14@users.noreply.github.com>
2017-07-01 21:28:36 -0500

    Fix to: https://github.com/mattyx14/otxserver/issues/287

--- path_10_9/data/events/scripts/player.lua
+++ path_10_9/data/events/scripts/player.lua
@@ -11,7 +11,56 @@ function Player:onBrowseField(position)
 end
 
 function Player:onLook(thing, position, distance)
-	local description = 'You see '
+	local description = "You see " .. thing:getDescription(distance)
+	if self:getGroup():getAccess() then
+		if thing:isItem() then
+			description = string.format("%s\nItem ID: %d", description, thing:getId())
+
+			local actionId = thing:getActionId()
+			if actionId ~= 0 then
+				description = string.format("%s, Action ID: %d", description, actionId)
+			end
+
+			local uniqueId = thing:getAttribute(ITEM_ATTRIBUTE_UNIQUEID)
+			if uniqueId > 0 and uniqueId < 65536 then
+				description = string.format("%s, Unique ID: %d", description, uniqueId)
+			end
+
+			local itemType = thing:getType()
+
+			local transformEquipId = itemType:getTransformEquipId()
+			local transformDeEquipId = itemType:getTransformDeEquipId()
+			if transformEquipId ~= 0 then
+				description = string.format("%s\nTransforms to: %d (onEquip)", description, transformEquipId)
+			elseif transformDeEquipId ~= 0 then
+				description = string.format("%s\nTransforms to: %d (onDeEquip)", description, transformDeEquipId)
+			end
+
+			local decayId = itemType:getDecayId()
+			if decayId ~= -1 then
+				description = string.format("%s\nDecays to: %d", description, decayId)
+			end
+		elseif thing:isCreature() then
+			local str = "%s\nHealth: %d / %d"
+			if thing:getMaxMana() > 0 then
+				str = string.format("%s, Mana: %d / %d", str, thing:getMana(), thing:getMaxMana())
+			end
+			description = string.format(str, description, thing:getHealth(), thing:getMaxHealth()) .. "."
+		end
+
+		local position = thing:getPosition()
+		description = string.format(
+			"%s\nPosition: %d, %d, %d",
+			description, position.x, position.y, position.z
+		)
+
+		if thing:isCreature() then
+			if thing:isPlayer() then
+				description = string.format("%s\nIP: %s.", description, Game.convertIpToString(thing:getIp()))
+			end
+		end
+	end
+
 	if thing:isItem() then
 		local itemType = thing:getType()
 		if (itemType and itemType:getImbuingSlots() > 0) then
@@ -43,53 +92,6 @@ function Player:onLook(thing, position, distance)
 		description = description .. thing:getDescription(distance)
 	end
 
-	if self:getGroup():getAccess() then
-		if thing:isItem() then
-			description = string.format('%s\nItem ID: %d', description, thing.itemid)
-
-			local actionId = thing.actionid
-			if actionId ~= 0 then
-				description = string.format('%s, Action ID: %d', description, actionId)
-			end
-
-			local uniqueId = thing:getAttribute(ITEM_ATTRIBUTE_UNIQUEID)
-			if uniqueId > 0 and uniqueId < 65536 then
-				description = string.format('%s, Unique ID: %d', description, uniqueId)
-			end
-
-			description = description .. '.'
-			local itemType = thing:getType()
-
-			local transformEquipId = itemType:getTransformEquipId()
-			local transformDeEquipId = itemType:getTransformDeEquipId()
-			if transformEquipId ~= 0 then
-				description = string.format('%s\nTransforms to: %d (onEquip)', description, transformEquipId)
-			elseif transformDeEquipId ~= 0 then
-				description = string.format('%s\nTransforms to: %d (onDeEquip)', description, transformDeEquipId)
-			end
-
-			local decayId = itemType:getDecayId()
-			if decayId ~= -1 then
-				description = string.format('%s\nDecays to: %d', description, decayId)
-			end
-		elseif thing:isCreature() then
-			local str = '%s\nHealth: %d / %d'
-			if thing:getMaxMana() > 0 then
-				str = string.format('%s, Mana: %d / %d', str, thing:getMana(), thing:getMaxMana())
-			end
-			description = string.format(str, description, thing:getHealth(), thing:getMaxHealth()) .. '.'
-		end
-
-		local position = thing:getPosition()
-		description = string.format(
-			'%s\nPosition: %d, %d, %d',
-			description, position.x, position.y, position.z
-		)
-
-		if thing:isCreature() and thing:isPlayer() then
-			description = string.format('%s\nIP: %s.', description, Game.convertIpToString(thing:getIp()))
-		end
-	end
 	self:sendTextMessage(MESSAGE_INFO_DESCR, description)
 end
 
