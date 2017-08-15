fe15d398a042e6c981acf33b60b9666f87b24c9b
Matt Gomez <mattyx14@users.noreply.github.com>
2017-08-15 18:37:59 -0500

    missing it

--- path_10_11/data/events/scripts/player.lua
+++ path_10_11/data/events/scripts/player.lua
@@ -124,6 +124,28 @@ function Player:onLook(thing, position, distance)
 	self:sendTextMessage(MESSAGE_INFO_DESCR, description)
 end
 
+function Player:onLookInBattleList(creature, distance)
+	local description = "You see " .. creature:getDescription(distance)
+	if self:getGroup():getAccess() then
+		local str = "%s\nHealth: %d / %d"
+		if creature:getMaxMana() > 0 then
+			str = string.format("%s, Mana: %d / %d", str, creature:getMana(), creature:getMaxMana())
+		end
+		description = string.format(str, description, creature:getHealth(), creature:getMaxHealth()) .. "."
+
+		local position = creature:getPosition()
+		description = string.format(
+			"%s\nPosition: %d, %d, %d",
+			description, position.x, position.y, position.z
+		)
+
+		if creature:isPlayer() then
+			description = string.format("%s\nIP: %s", description, Game.convertIpToString(creature:getIp()))
+		end
+	end
+	self:sendTextMessage(MESSAGE_INFO_DESCR, description)
+end
+
 function Player:onLookInTrade(partner, item, distance)
 	self:sendTextMessage(MESSAGE_INFO_DESCR, "You see " .. item:getDescription(distance))
 end
