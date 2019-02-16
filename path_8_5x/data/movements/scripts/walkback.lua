78a448450c5e60c63c8cf843e1a79964957f61c1
Matt Gomez <mattyx14@users.noreply.github.com>
2019-02-15 18:37:56 -0600

    extras to fixes

--- path_8_5x/data/movements/scripts/walkback.lua
+++ path_8_5x/data/movements/scripts/walkback.lua
@@ -1,14 +1,18 @@
 local SPECIAL_QUESTS = {2001}
 
 function onStepIn(cid, item, position, lastPosition, fromPosition, toPosition, actor)
-	local container = isContainer(item.uid)
-	if((container and not isInArray(SPECIAL_QUESTS, item.actionid) and
-		item.uid > 65535) or (fromPosition.x ~= 0 and getTileInfo(fromPosition).floorChange[9])
-		or (not container and getTileInfo(position).creatures <= 1)) then
+	if(not isPlayer(cid)) then
 		return true
 	end
 
-	if(lastPosition.x == 0) then -- player just logged in
+	local container = isContainer(item.uid)
+	if((container and not isInArray(SPECIAL_QUESTS, item.actionid) and
+		item.uid > 65535) or (not container and getTileInfo(position).creatures <= 1)
+		or (fromPosition.x ~= 0 and getTileInfo(fromPosition).floorChange[9])) then
+		return true
+	end
+
+	if(fromPosition.x == 0) then -- player just logged in
 		lastPosition = getTownTemplePosition(getPlayerTown(cid))
 		doSendMagicEffect(lastPosition, CONST_ME_TELEPORT)
 	end
