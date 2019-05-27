b652895bcb28e2d30b5210d0e76a64a052177426
Danyel Varejão <30702437+danielsalim1@users.noreply.github.com>
2019-05-27 09:39:55 -0300

    Problem in NPC Trade with infinity cap flag.
    The bug works only in patch 8.6.
    http://prntscr.com/ntx3ia

--- path_8_6/src/player.h
+++ path_8_6/src/player.h
@@ -429,7 +429,7 @@ class Player final : public Creature, public Cylinder
 			if (hasFlag(PlayerFlag_CannotPickupItem)) {
 				return 0;
 			} else if (hasFlag(PlayerFlag_HasInfiniteCapacity)) {
-				return std::numeric_limits<uint32_t>::max();
+				return 10000;
 			} else {
 				return std::max<int32_t>(0, capacity - inventoryWeight);
 			}
