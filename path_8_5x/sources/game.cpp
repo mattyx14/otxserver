821c850166abb4fa0adc26e736e8e31bf8021ac2
Reason <matheus_matias_ferreira@hotmail.com>
2019-12-09 21:26:48 -0300

    Little fix for doCreatureSetNoMove
    *This should allow better 'script control' on a creature if the function is set to true

--- path_8_5x/sources/game.cpp
+++ path_8_5x/sources/game.cpp
@@ -1274,7 +1274,7 @@ bool Game::playerMoveCreature(uint32_t playerId, uint32_t movingCreatureId,
 
 ReturnValue Game::internalMoveCreature(Creature* creature, Direction direction, uint32_t flags/* = 0*/)
 {
-	if (creature->getNoMove())
+	if (creature->getNoMove() && creature->getType() == CREATURETYPE_MONSTER)
 		return RET_NOTPOSSIBLE;
 	
 	const Position& currentPos = creature->getPosition();
@@ -1328,9 +1328,6 @@ ReturnValue Game::internalMoveCreature(Creature* actor, Creature* creature, Cyli
 	ReturnValue ret = toCylinder->__queryAdd(0, creature, 1, flags, actor);
 	if(ret != RET_NOERROR)
 		return ret;
-	
-	if (creature->getNoMove())
-		return RET_NOTPOSSIBLE;
 
 	fromCylinder->getTile()->moveCreature(actor, creature, toCylinder, forceTeleport);
 	if(creature->getParent() != toCylinder)
