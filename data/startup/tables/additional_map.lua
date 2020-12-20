3c817a4685ade483dfc65bd191605560d3462f43
Matt Gomez <mattyx14@users.noreply.github.com>
2020-12-19 19:04:11 -0600

    fixes and misses

--- data/startup/tables/additional_map.lua
+++ data/startup/tables/additional_map.lua
@@ -1,9 +1,11 @@
 CustomMapTable = {
 	-- Custom otservbr map
-	[1] = {
-		mapName = "otservbr-custom",
-		mapFile = "data/world/custom/otservbr-custom.otbm",
-		spawnFile = "data/world/custom/otservbr-custom-spawn.xml",
-		enabled = true
-	}
+	--[[
+		[1] = {
+			mapName = "otservbr-custom",
+			mapFile = "data/world/custom/otservbr-custom.otbm",
+			spawnFile = "data/world/custom/otservbr-custom-spawn.xml",
+			enabled = true
+		}
+	]]
 }
