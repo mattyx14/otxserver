0f70b5f700c964df0d015e19f1eed1709ffb797c
Reason <matheus_matias_ferreira@hotmail.com>
2021-07-12 01:16:46 -0300

    Update on std::put_time
    *This should allow even more systems to work fine on OTX2

--- sources/tools.cpp
+++ sources/tools.cpp
@@ -19,6 +19,55 @@
 
 #include <iostream>
 #include <iomanip>
+#include <ctime>
+
+
+//std::put_time for old GCC
+#if __GNUC__ && __GNUC__ < 5
+#include <ostream>  // std::basic_ostream
+#include <ios>      // std::ios_base
+#include <locale>   // std::use_facet, std::time_put
+#include <iterator> // std::ostreambuf_iterator
+
+template<typename CharT>
+struct _put_time
+{
+	const std::tm* time;
+	const char* fmt;
+};
+
+template<typename CharT>
+inline _put_time<CharT>
+put_time(const std::tm* time, const CharT* fmt)
+{
+	return { time, fmt };
+}
+
+template<typename CharT, typename Traits>
+std::basic_ostream<CharT, Traits>&
+operator<<(std::basic_ostream<CharT, Traits>& os, _put_time<CharT> f)
+{
+	typedef typename std::ostreambuf_iterator<CharT, Traits> Iter;
+	typedef std::time_put<CharT, Iter> TimePut;
+
+	const CharT* const fmt_end = f.fmt + Traits::length(f.fmt);
+	const TimePut& mp = std::use_facet<TimePut>(os.getloc());
+
+	std::ios_base::iostate err = std::ios_base::goodbit;
+	try {
+		if(mp.put(Iter(os.rdbuf()), os, os.fill(), f.time, f.fmt, fmt_end).failed())
+			err |= std::ios_base::badbit;
+	} catch(...) {
+		err |= std::ios_base::badbit;
+	}
+
+	if(err)
+		os.setstate(err);
+
+	return os;
+}
+
+#endif
 
 #include <boost/filesystem.hpp>
 #include "sha1.h"
