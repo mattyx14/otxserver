e4eb2f089f530985efbca00dd561264efdb2dfd9
Matt Gomez <mattyx14@users.noreply.github.com>
2022-02-14 13:34:01 -0600

    Update partyAndGuildProtection.lua
    Adrian
    https://github.com/mattyx14/otxserver/issues/698

--- data/creaturescripts/scripts/partyAndGuildProtection.lua
+++ data/creaturescripts/scripts/partyAndGuildProtection.lua
@@ -1,12 +1,12 @@
 function onCombat(cid, target)
 	if(isPlayer(cid) and isPlayer(target)) then
 		if(getConfigValue("noDamageToGuildMates")) then
-			if(getPlayerGuildId(cid) == getPlayerGuildId(target)) then
+			if isPlayer(target) and getPlayerGuildId(cid) ~= 0 and getPlayerGuildId(cid) == getPlayerGuildId(target) then
 				return false
 			end
 		end
 		if(getConfigValue("noDamageToPartyMembers")) then
-			if(getPlayerParty(cid) == getPlayerParty(target)) then
+			if isInParty(cid) and isInParty(target) and (getPlayerParty(cid) == getPlayerParty(target)) then
 				return false
 			end
 		end
