05711462106ac7a16bbd169deaa9ca69e6534f36
Matt Gomez <martin_jojo@hotmail.com>
2024-02-14 12:00:21 -0600

    update : https://github.com/opentibiabr/canary/commit/17719cf5b07479fba0a5fea5ea77f85f90aa3189

--- data-otxserver/lib/core/load.lua
+++ data-otxserver/lib/core/load.lua
@@ -1,3 +1,4 @@
 dofile(DATA_DIRECTORY .. "/lib/core/storages.lua")
+dofile(DATA_DIRECTORY .. "/lib/core/constants.lua")
 dofile(DATA_DIRECTORY .. "/lib/core/storagesCustom.lua")
 dofile(DATA_DIRECTORY .. "/lib/core/quests.lua")
