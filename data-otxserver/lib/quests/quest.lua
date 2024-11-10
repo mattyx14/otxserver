4a2a70e32f680f72a7c30342220d7c4e016c082d
mattyx14 <martin_jojo@hotmail.com>
2024-11-10 14:57:57 -0600

    console error

--- data-otxserver/lib/quests/quest.lua
+++ data-otxserver/lib/quests/quest.lua
@@ -1,2 +1,3 @@
 dofile(DATA_DIRECTORY .. "/lib/quests/killing_in_the_name_of.lua")
 dofile(DATA_DIRECTORY .. "/lib/quests/the_primal_ordeal.lua")
+dofile(DATA_DIRECTORY .. "/lib/quests/soul_war.lua")
