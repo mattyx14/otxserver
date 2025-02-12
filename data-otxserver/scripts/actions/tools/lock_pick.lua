cbcdb70d2913b56d6cb17f624e89ba417bae0fd4
mattyx14 <martin_jojo@hotmail.com>
2025-02-12 15:12:17 -0600

    update : https://github.com/opentibiabr/canary/commit/ab3275444631dbdc558456637002c4487d97f7de

--- data-otxserver/scripts/actions/tools/lock_pick.lua
+++ data-otxserver/scripts/actions/tools/lock_pick.lua
@@ -1,10 +1,6 @@
 local lockPick = Action()
 
 function lockPick.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if target.actionid ~= 12503 then
-		return false
-	end
-
 	return true
 end
 
